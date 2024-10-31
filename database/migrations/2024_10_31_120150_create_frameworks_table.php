f077d259009af71b24275fbddfaf68b6227c1c20
Trey Bastian <2991824+TreyBastian@users.noreply.github.com>
2024-10-31 13:07:03 +0000

    clarification

--- database/migrations/2024_10_31_120150_create_frameworks_table.php
+++ database/migrations/2024_10_31_120150_create_frameworks_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('frameworks', function (Blueprint $table) {
-            $table->uuid()->primary();
+            $table->uuid('id')->primary();
             $table->text('name')->unique();
             $table->timestamps();
         });
--- database/migrations/2024_10_31_120452_create_locations_table.php
+++ database/migrations/2024_10_31_120452_create_locations_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('locations', function (Blueprint $table) {
-            $table->uuid()->primary();
+            $table->uuid('id')->primary();
             $table->text('name')->unique();
             $table->timestamps();
         });
